021841f5d4ede22e8dbdc9f5997acbceb1283a55
alex <alex@webgoal.ru>
2024-06-04 11:49:03 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,13 @@ void func2()
     std::cout << "Working " << __FUNCTION__ << "...\n";
 }
 
-void addF1(thread_pool & tr_p)
+void addF1(thread_pool & tr_p, std::function<void(void)>&& f)
 {
     for (size_t i = 0; i < 5; i++)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
         std::lock_guard l(safe_queue::m);
-        tr_p.submit(func1);
+        tr_p.submit(f);
     }
 }
 
@@ -39,6 +39,6 @@ int main(int, char**)
 {
     thread_pool tr_p;
     // Заполняем очередь задачами
-    std::thread th1(addF1, tr_p);
+    std::thread th1(addF1, tr_p, func1);
     // std::thread th2(addF2, tr_p);
 }
