44a4e4b57806c649d9b0871c4efaf93eb4dd970c
alex <alex@webgoal.ru>
2024-06-13 10:43:31 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "thread_pool.h"
 
-// std::queue<std::function<void(void)>> wor_queue;
+std::queue<std::function<void(void)>> wor_queue;
 void func1()
 {
     std::this_thread::sleep_for(std::chrono::milliseconds(200));
