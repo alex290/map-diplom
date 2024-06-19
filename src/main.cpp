09ebf3e97bc2487a78cfa93c0b04026a1d864897
alex <alex@webgoal.ru>
2024-06-19 10:54:44 +0300

    Update

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,46 @@
 #include "thread_pool.h"
 
-thread_pool<std::function<void(void)>> tr_p;
-
 void func1()
 {
     std::cout << "Working " << __FUNCTION__ << "…\n";
 }
 
-void func2()
-{
-    std::cout << "Working " << __FUNCTION__ << "…\n";
-}
+// void func2()
+// {
+//     std::cout << "Working " << __FUNCTION__ << "…\n";
+// }
 
-void addF1()
+void addF1(thread_pool<std::function<void(void)>>* tr_p, std::function<void(void)> &f)
 {
     for (size_t i = 0; i < 5; i++)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        tr_p.submit(func1);
+        tr_p->submit(std::move(f));
     }
 }
 
-void addF2()
-{
-    for (size_t i = 0; i < 5; i++)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        tr_p.submit(func2);
-    }
-}
+// void addF2(thread_pool<std::function<void(void)>>* tr_p)
+// {
+//     for (size_t i = 0; i < 5; i++)
+//     {
+//         std::this_thread::sleep_for(std::chrono::milliseconds(500));
+//         tr_p.submit(func2);
+//     }
+// }
 
 void work()
 {
-    tr_p.work();
+    // tr_p.work();
 }
 
 int main(int, char**)
 {
-    std::thread t1(addF1);
-    std::thread t2(addF2);
-    std::thread t3(work);
+    thread_pool<std::function<void(void)>>* tr_p = new thread_pool<std::function<void(void)>>;
+    std::thread t1(addF1, tr_p, func1);
+    // std::thread t2(addF2, tr_p);
+    // // std::thread t3(work);
     t1.join();
-    t2.join();
-    t3.join();
+    // t2.join();
+    // t3.join();
+    delete tr_p;
 }
