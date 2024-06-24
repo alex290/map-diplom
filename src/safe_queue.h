8177abf3d70c9a19dc5f6fe0e9ac20f42ccf2468
alex <alex@webgoal.ru>
2024-06-24 17:24:10 +0300

    Update

--- src/safe_queue.h
+++ src/safe_queue.h
@@ -26,8 +26,10 @@ public:
     }
     auto pop()
     {
-        std::lock_guard<std::mutex> lk(m);
-        data_cond.wait(lk, [this]{ return !wor_queue.empty(); });
+        // std::lock_guard<std::mutex> lk(m);
+        std::unique_lock<std::mutex> lock(m);
+        data_cond.wait(lock, [this]{ return !wor_queue.empty(); });
+        // while (!is_empty()) data_cond.wait(lock);
         auto task = wor_queue.front();
         // auto ret = task();
         wor_queue.pop();
